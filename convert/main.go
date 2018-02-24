4b15fad8c23b4086dae4c244a27ca475f1942846
Yong Kang <yong.kang@oakridge.io>
2018-02-24 14:54:55 -0800

    merge to one file

--- convert/main.go
+++ convert/main.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "os"
+    "net"
     "strings"
     "sync"
     "image_burner/util"
@@ -11,7 +12,7 @@ import (
 
 /*
  * global vars
-*/
+ */
 var netlist []Subnet
 const Banner_start = "\nOakridge Firmware Update Utility, Ver 1.01, (c) Oakridge Networks, Inc. 2018\n"
 const Banner_end = "\nThanks for choose Oakridge Networks Inc.\n"
@@ -32,6 +33,59 @@ var img = map[string][]string {
     AC_PRO:  {"oakridge.sysloader.ubnt.tar.gz", "http://image.oakridge.vip:8000/images/ap/ubntunifi/sysloader/latest-sysupgrade.bin.tar.gz"},
 }
 
+
+
+type Subnet struct {
+    Net             string
+    holes           []net.IP                        // skip those ip-addr
+    Oak_dev_list    []*oakUtility.Oakridge_Device
+    UBNT_ap_list    []*oakUtility.UBNT_AP
+    batch           sync.WaitGroup                   // this to wait all host finish before exit
+}
+
+func New_Subnet (cidr string) Subnet {
+    return Subnet { Net: cidr }
+}
+func (s *Subnet) Holes (h []net.IP) {
+    s.holes = h
+}
+
+func (s *Subnet) Scan () {
+    hosts,err := oakUtility.Net2hosts_exclude (s.Net, s.holes)
+    if err != nil {
+        fmt.Println(s.Net,err)
+        return
+    }
+
+    // spinner
+    p := spinner.StartNew(s.Net)
+    defer func () {
+        p.Stop ()
+    } ()
+
+    // do all hosts in one subnet in a parallel
+    for _, h := range hosts {
+        s.batch.Add(1)
+        go s.scan_one (h)
+    }
+    s.batch.Wait()
+}
+func (s *Subnet) scan_one (host string) {
+
+    defer s.batch.Done()
+
+    c := oakUtility.New_SSHClient (host)
+
+    if dev := c.Is_oakridge_dev(); dev != nil {
+            s.Oak_dev_list = append(s.Oak_dev_list, dev)
+    } else if dev := c.Is_ubnt_ap(); dev != nil {
+            s.UBNT_ap_list = append(s.UBNT_ap_list, dev)
+    }
+}
+
+func (s *Subnet) OneLineSummary () {
+    fmt.Printf("✓ %s: Completed, %d Oakridge, %d UBNT devices\n",s.Net,len(s.Oak_dev_list),len(s.UBNT_ap_list))
+}
 
 func list_scan_result () {
     cnt := 0
@@ -155,6 +209,23 @@ func install_oak_firmwire () {
 }
 
 
+func scan_local_subnet () {
+    nets, selfs, err := oakUtility.Get_local_subnets()
+    if err != nil {
+        log.Error.Fatalln(err)
+    }
+
+    println("Scanning local networks ...\n")
+
+    // scan each subnet
+    for _, net := range nets {
+        n := New_Subnet (net)
+        n.Holes (selfs)
+        n.Scan ()
+        n.OneLineSummary()
+        netlist =  append(netlist, n)
+    }
+}
 
 func init () {
     log = oakUtility.New_OakLogger()
